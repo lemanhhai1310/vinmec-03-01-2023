5f88b22527b4ae1987525181ce91228c0b1c27b1
hailm <hai.lemanh.1310@gmail.com>
2023-01-03 13:11:41 +0700

    feat: update

---
+++ danhsach.php
@@ -0,0 +1,5 @@
+<?php $data["title"] = "2. CKS - Danh sách văn bản cần ký"; ?>
+<?php $bodyClass = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+
+<?php require "template-parts/layouts/footer.php"; ?>
---
+++ taochuky.php
@@ -0,0 +1,5 @@
+<?php $data["title"] = "3. CKS - Tạo chữ ký"; ?>
+<?php $bodyClass = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+
+<?php require "template-parts/layouts/footer.php"; ?>
